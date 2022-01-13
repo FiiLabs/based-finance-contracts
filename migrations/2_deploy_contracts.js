13e468ae5afed02834e8a0aefcc4b9f2f1c78ebf
David Guras <gura.san@gmail.com>
2022-01-13 10:37:26 -0500

    Edit contracts and truffle deploy script
    * add missing SPDX-License-Identifier
    * remove visability specifier from constructors
    * add .env require statement to truffle-config
    * add .env var to deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,95 @@
-//const Distributor = artifacts.require("Distributor");
-//const DummyToken = artifacts.require("DummyToken");
-//const Masonry = artifacts.require("Masonry");
-//const Migrations = artifacts.require("Migrations");
-//const Oracle = artifacts.require("Oracle");
-//const SimpleERCFund = artifacts.require("SimpleERCFund");
-//const TShare = artifacts.require("TShare");
-//const TBond = artifacts.require("TBond");
-//const TaxOffice = artifacts.require("TaxOffice");
+const Distributor = artifacts.require("Distributor");
+const DummyToken = artifacts.require("DummyToken");
+const Masonry = artifacts.require("Masonry");
+const Migrations = artifacts.require("Migrations");
+const Oracle = artifacts.require("Oracle");
+const SimpleERCFund = artifacts.require("SimpleERCFund");
+const TBond = artifacts.require("TBond");
+const TaxOffice = artifacts.require("TaxOffice");
 //const TaxOfficeV2 = artifacts.require("TaxOfficeV2");
-//const TaxOracle = artifacts.require("TaxOracle");
-//const Timelock = artifacts.require("Timelock");
+const TaxOracle = artifacts.require("TaxOracle");
+const Timelock = artifacts.require("Timelock");
 const Tomb = artifacts.require("Tomb");
-//const Treasury = artifacts.require("Treasury");
+const Treasury = artifacts.require("Treasury");
+const TShare = artifacts.require("TShare");
+/*
+module.exports = async function(deployer) {
+  // Deploy DummyToken Contract
+  await deployer.deploy(DummyToken);
+  const tomb = await DummyToken.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy Masonry Contracts
+  await deployer.deploy(Masonry);
+  const tomb = await Masonry.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy Migrations Contract
+  await deployer.deploy(Migrations);
+  const tomb = await Migrations.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy Oracle Contract
+  await deployer.deploy(Oracle);
+  const tomb = await Oracle.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy Oracle Contract
+  await deployer.deploy(Oracle);
+  const tomb = await Oracle.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy SimpleERCFund Contract
+  await deployer.deploy(SimpleERCFund);
+  const tomb = await SimpleERCFund.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy TBond Contract
+  await deployer.deploy(TBond);
+  const tomb = await TBond.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy TaxOffice Contract
+  await deployer.deploy(TaxOffice);
+  const tomb = await TaxOffice.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy TaxOracle Contract
+  await deployer.deploy(TaxOracle);
+  const tomb = await TaxOracle.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy Timelock Contract
+  await deployer.deploy(Timelock);
+  const tomb = await Timelock.deployed();
+};*/
 
 module.exports = async function(deployer, _taxRate, _taxCollectorAddress) {
-  // Deploy Distributor Contract
-  await deployer.deploy(Tomb, 1000, "0x1dB3720E5B5AD9B7F05E49f3DeBA2eaB6afD293c");
+  // Deploy Tomb Contract
+  await deployer.deploy(Tomb, 1000, process.env.TAXCOLLECTORADR);
   const tomb = await Tomb.deployed();
 };
+
+/*
+module.exports = async function(deployer, _taxRate, _taxCollectorAddress) {
+  // Deploy Treasury Contract
+  await deployer.deploy(Treasury);
+  const tomb = await Treasury.deployed();
+};
+
+module.exports = async function(deployer) {
+  // Deploy TShare Contract
+  await deployer.deploy(TShare);
+  const tomb = await TShare.deployed();
+};
+*/
+
