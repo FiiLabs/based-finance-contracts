506f3234b58afcad535447e80adfb8b91f16011c
David Guras <gura.san@gmail.com>
2022-02-06 08:45:24 -0500

    Refactor Based.sol
    * tweak tokenomics
    * remove taxation
    * remove TODOs

--- contracts/Based.sol
+++ contracts/Based.sol
@@ -21,109 +21,31 @@ contract Based is ERC20Burnable, Operator {
     using SafeMath8 for uint8;
     using SafeMath for uint256;
 
-    // Initial distribution for the first 24h genesis pools
-    uint256 public constant INITIAL_GENESIS_POOL_DISTRIBUTION = 32000 ether;
+    // Initial distribution for the first 48h genesis pools
     // total of based we pay to users during genesis
-    // Initial distribution for the day 2-5 BASED-TOMB LP -> BASED pool
-    // WE WANT THIS POOL TO RUN SAME AMOUNT OF TIME AS GENESIS REWARD POOL....
-    uint256 public constant INITIAL_TREASURY_DISTRIBUTION = 1000 ether; // native lp pool that pays in based to users
-    // Distribution for airdrops wallet
-    // uint256 public constant INITIAL_AIRDROP_WALLET_DISTRIBUTION = 9000 ether;
+    uint256 public constant INITIAL_GENESIS_POOL_DISTRIBUTION = 27500 ether;
+
+    // DAO FUND INITIAL ALLOCATION IS 1000 BASED
+    uint256 public constant INITIAL_DAOFUND_DISTRIBUTION = 1000 ether;
+
 
     // Have the rewards been distributed to the pools
     bool public rewardPoolDistributed = false;
 
-    /* ================= Taxation =============== */
+
     // Address of the Oracle
     address public basedOracle;
-    // Address of the Tax Office
-    address public taxOffice;
-
-    // Current tax rate
-    uint256 public taxRate;
-    // Price threshold below which taxes will get burned
-    uint256 public burnThreshold = 1.10e18;
-    // Address of the tax collector wallet
-    address public taxCollectorAddress;
-
-    // Should the taxes be calculated using the tax tiers
-    bool public autoCalculateTax;
-
-    // Tax Tiers
-    uint256[] public taxTiersTwaps = [0, 5e17, 6e17, 7e17, 8e17, 9e17, 9.5e17, 1e18, 1.05e18, 1.10e18, 1.20e18, 1.30e18, 1.40e18, 1.50e18];
-    uint256[] public taxTiersRates = [2000, 1900, 1800, 1700, 1600, 1500, 1500, 1500, 1500, 1400, 900, 400, 200, 100];
-
-    // Sender addresses excluded from Tax
-    mapping(address => bool) public excludedAddresses;
-
-    event TaxOfficeTransferred(address oldAddress, address newAddress);
-
-    modifier onlyTaxOffice() {
-        require(taxOffice == msg.sender, "Caller is not the tax office");
-        _;
-    }
-
-    modifier onlyOperatorOrTaxOffice() {
-        require(isOperator() || taxOffice == msg.sender, "Caller is not the operator or the tax office");
-        _;
-    }
 
     /**
      * @notice Constructs the BASED ERC-20 contract.
      */
-    constructor(uint256 _taxRate, address _taxCollectorAddress) ERC20("testBSD", "testBSD") {
-        // Mints 1 BASED to contract creator for initial pool setup
-        require(_taxRate < 10000, "tax equal or bigger to 100%");
-        require(_taxCollectorAddress != address(0), "tax collector address must be non-zero address");
+    constructor() ERC20("BASED", "BASED") {
+        // Mints 5000 BASED to contract creator for initial pool setup
 
-        excludeAddress(address(this));
+        _mint(msg.sender, 5000 ether);
 
-        _mint(msg.sender, 5000 ether);    // WE MINT 5000 BASED RIGHT AWAY FOR INITIAL LP CREATION
-        taxRate = _taxRate;
-        taxCollectorAddress = _taxCollectorAddress;
     }
 
-    /* ============= Taxation ============= */
-
-    function getTaxTiersTwapsCount() public view returns (uint256 count) {
-        return taxTiersTwaps.length;
-    }
-
-    function getTaxTiersRatesCount() public view returns (uint256 count) {
-        return taxTiersRates.length;
-    }
-
-    function isAddressExcluded(address _address) public view returns (bool) {
-        return excludedAddresses[_address];
-    }
-
-    function setTaxTiersTwap(uint8 _index, uint256 _value) public onlyTaxOffice returns (bool) {
-        require(_index >= 0, "Index has to be higher than 0");
-        require(_index < getTaxTiersTwapsCount(), "Index has to lower than count of tax tiers");
-        if (_index > 0) {
-            require(_value > taxTiersTwaps[_index - 1]);
-        }
-        if (_index < getTaxTiersTwapsCount().sub(1)) {
-            require(_value < taxTiersTwaps[_index + 1]);
-        }
-        taxTiersTwaps[_index] = _value;
-        return true;
-    }
-
-    function setTaxTiersRate(uint8 _index, uint256 _value) public onlyTaxOffice returns (bool) {
-        require(_index >= 0, "Index has to be higher than 0");
-        require(_index < getTaxTiersRatesCount(), "Index has to lower than count of tax tiers");
-        taxTiersRates[_index] = _value;
-        return true;
-    }
-
-    function setBurnThreshold(uint256 _burnThreshold) public onlyTaxOffice returns (bool) {
-        burnThreshold = _burnThreshold;
-        //TODO Figure out if return needed
-        return true;
-    }
-
-    //TODO Figure out if return is correct
     function _getBasedPrice() internal view returns (uint256 _basedPrice) {
         try IOracle(basedOracle).consult(address(this), 1e18) returns (uint144 _price) {
             return uint256(_price);
@@ -132,58 +54,9 @@ contract Based is ERC20Burnable, Operator {
         }
     }
 
-    function _updateTaxRate(uint256 _basedPrice) internal returns (uint256){
-        if (autoCalculateTax) {
-            for (uint8 tierId = uint8(getTaxTiersTwapsCount()).sub(1); tierId >= 0; --tierId) {
-                if (_basedPrice >= taxTiersTwaps[tierId]) {
-                    require(taxTiersRates[tierId] < 10000, "tax equal or bigger to 100%");
-                    taxRate = taxTiersRates[tierId];
-                    return taxTiersRates[tierId];
-                }
-            }
-        }
-    }
-
-    function enableAutoCalculateTax() public onlyTaxOffice {
-        autoCalculateTax = true;
-    }
-
-    function disableAutoCalculateTax() public onlyTaxOffice {
-        autoCalculateTax = false;
-    }
-
-    function setBasedOracle(address _basedOracle) public onlyOperatorOrTaxOffice {
+    function setBasedOracle(address _basedOracle) public onlyOperator {
         require(_basedOracle != address(0), "oracle address cannot be 0 address");
         basedOracle = _basedOracle;
-    }
-
-    function setTaxOffice(address _taxOffice) public onlyOperatorOrTaxOffice {
-        require(_taxOffice != address(0), "tax office address cannot be 0 address");
-        emit TaxOfficeTransferred(taxOffice, _taxOffice);
-        taxOffice = _taxOffice;
-    }
-
-    function setTaxCollectorAddress(address _taxCollectorAddress) public onlyTaxOffice {
-        require(_taxCollectorAddress != address(0), "tax collector address must be non-zero address");
-        taxCollectorAddress = _taxCollectorAddress;
-    }
-
-    function setTaxRate(uint256 _taxRate) public onlyTaxOffice {
-        require(!autoCalculateTax, "auto calculate tax cannot be enabled");
-        require(_taxRate < 10000, "tax equal or bigger to 100%");
-        taxRate = _taxRate;
-    }
-
-    function excludeAddress(address _address) public onlyOperatorOrTaxOffice returns (bool) {
-        require(!excludedAddresses[_address], "address can't be excluded");
-        excludedAddresses[_address] = true;
-        return true;
-    }
-
-    function includeAddress(address _address) public onlyOperatorOrTaxOffice returns (bool) {
-        require(excludedAddresses[_address], "address can't be included");
-        excludedAddresses[_address] = false;
-        return true;
     }
 
     /**
@@ -213,48 +86,8 @@ contract Based is ERC20Burnable, Operator {
         address recipient,
         uint256 amount
     ) public override returns (bool) {
-        uint256 currentTaxRate = 0;
-        bool burnTax = false;
-
-        if (autoCalculateTax) {
-            uint256 currentBasedPrice = _getBasedPrice();
-            currentTaxRate = _updateTaxRate(currentBasedPrice);
-            if (currentBasedPrice < burnThreshold) {
-                burnTax = true;
-            }
-        }
-
-
-        if (currentTaxRate == 0 || excludedAddresses[sender]) {
-            _transfer(sender, recipient, amount);
-        } else {
-            _transferWithTax(sender, recipient, amount, burnTax);
-        }
-
+        _transfer(sender, recipient, amount);
         _approve(sender, _msgSender(), allowance(sender, _msgSender()).sub(amount, "ERC20: transfer amount exceeds allowance"));
-        return true;
-    }
-
-    function _transferWithTax(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bool burnTax
-    ) internal returns (bool) {
-        uint256 taxAmount = amount.mul(taxRate).div(10000);
-        uint256 amountAfterTax = amount.sub(taxAmount);
-
-        if(burnTax) {
-            // Burn tax
-            super.burnFrom(sender, taxAmount);
-        } else {
-            // Transfer tax to tax collector
-            _transfer(sender, taxCollectorAddress, taxAmount);
-        }
-
-        // Transfer amount after tax to recipient
-        _transfer(sender, recipient, amountAfterTax);
-
         return true;
     }
 
@@ -263,17 +96,17 @@ contract Based is ERC20Burnable, Operator {
      */
     function distributeReward(
         address _genesisPool,
-        address _treasury
-        // address _airdropWallet
+        address _daoFund
+
     ) external onlyOperator {
         require(!rewardPoolDistributed, "only can distribute once");
         require(_genesisPool != address(0), "!_genesisPool");
-        require(_treasury != address(0), "!_treasury");
-        // require(_airdropWallet != address(0), "!_airdropWallet");
+        require(_daoFund != address(0), "!_treasury");
+
         rewardPoolDistributed = true;
         _mint(_genesisPool, INITIAL_GENESIS_POOL_DISTRIBUTION);
-        _mint(_treasury, INITIAL_TREASURY_DISTRIBUTION);
-        // _mint(_airdropWallet, INITIAL_AIRDROP_WALLET_DISTRIBUTION);
+        _mint(_daoFund, INITIAL_DAOFUND_DISTRIBUTION);
+
     }
 
     function governanceRecoverUnsupported(
