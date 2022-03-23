8266817f119743e20e3d03cad5dcc7ba3f65b24f
David Guras <gura.san@gmail.com>
2022-03-23 11:41:05 -0400

    contract update

--- contracts/ProfitDistribution.sol
+++ contracts/ProfitDistribution.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -10,13 +10,14 @@ contract ProfitDistribution is Operator {
 
     string public name = "ProfitDistribution"; // call it ProfitDistribution
 
-
-
     IERC20 public depositToken;
     address public burnAddress;
     uint256 public totalStaked;
     uint256 public depositFee;
     uint256 public totalBurned;
+
+    //uint256[] public lockMultiplers; for later usage
+    uint256 public totalAllocation;
 
     address[] public stakers;
 
@@ -25,16 +26,26 @@ contract ProfitDistribution is Operator {
         uint256 rewardsPerEpoch;
         uint256 totalRewards;
         bool isActive;
+        uint256 distributedAmount;
+        uint256 LastDistributedAmountPerAlloc;
+        uint256[] rewardPerAllocHistory;
     }
 
     struct UserInfo {
         uint256 balance;
+        uint256 allocation;
         bool hasStaked;
         bool isStaking;
+
+        mapping(uint256=> uint256) lastSnapShotIndex; // Maps rewardPoolId to lastSnapshotindex
         mapping(uint256 => uint256) pendingRewards; // Maps rewardPoolId to amount
     }
 
+
     RewardInfo[] public rewardInfo;
+
+
+
     mapping(address => UserInfo) public userInfo;
 
     // in constructor pass in the address for reward token 1 and reward token 2
@@ -70,6 +81,36 @@ contract ProfitDistribution is Operator {
     event PendingRewardClaimed(address _user);
 
 
+    //update pending rewards modifier
+    modifier updatePendingRewards(address _sender){
+
+        UserInfo storage user = userInfo[_sender];
+
+        for(uint256 i = 0; i < rewardInfo.length; ++i){
+            RewardInfo storage reward = rewardInfo[i];
+
+            //calculate pending rewards
+            user.pendingRewards[i] = earned(_sender, i);
+            user.lastSnapShotIndex[i] = reward.rewardPerAllocHistory.length -1;
+        }
+
+        _;
+    }
+
+    /*this function calculates the earnings of user over the last recorded
+    epoch  to the most recent epoch using average rewardPerAllocation over time*/
+
+    function earned(address _sender, uint256 _rewardId) public view returns (uint256) {
+
+        UserInfo storage user = userInfo[_sender];
+        RewardInfo storage reward = rewardInfo[_rewardId];
+
+        uint256 latestRPA = reward.LastDistributedAmountPerAlloc;
+        uint256 storedRPA = reward.rewardPerAllocHistory[user.lastSnapShotIndex[_rewardId]];
+
+        return user.allocation*(latestRPA - storedRPA)/(1e18)+ user.pendingRewards[_rewardId];
+    }
+
     //update deposit fee
 
     function updateDepositFee(uint256 _depositFee) external onlyOperator {
@@ -84,7 +125,10 @@ contract ProfitDistribution is Operator {
         token: _token,
         rewardsPerEpoch: 0,
         totalRewards: 0,
-        isActive: false
+        isActive: false,
+        distributedAmount:0,
+        LastDistributedAmountPerAlloc:0,
+        rewardPerAllocHistory: new uint256[](1)
         }));
 
         emit AddReward(_token);
@@ -156,7 +200,7 @@ contract ProfitDistribution is Operator {
         reward.token.safeTransfer(msg.sender, _amount);
     }
 
-    function stakeTokens(uint256 _amount) external {
+    function stakeTokens(uint256 _amount) external updatePendingRewards(msg.sender){
         address _sender = msg.sender;
         UserInfo storage user = userInfo[_sender];
 
@@ -167,11 +211,15 @@ contract ProfitDistribution is Operator {
         uint256 depositAmount = _amount - feeAmount;
 
         //update totalBurned
-        totalBurned += totalBurned;
+        totalBurned += feeAmount;
 
         // Update the staking balance in map
         user.balance += depositAmount;
         emit UserStakedIncrease(_sender, depositAmount);
+
+        //update allocation
+        user.allocation += depositAmount;
+        totalAllocation += depositAmount;
 
         //update TotalStaked
         totalStaked += depositAmount;
@@ -194,7 +242,7 @@ contract ProfitDistribution is Operator {
     }
 
     // allow user to unstake total balance and withdraw USDC from the contract
-    function unstakeTokens(uint256 _amount) external {
+    function unstakeTokens(uint256 _amount) external updatePendingRewards(msg.sender) {
         address _sender = msg.sender;
         UserInfo storage user = userInfo[_sender];
 
@@ -206,6 +254,10 @@ contract ProfitDistribution is Operator {
         //update user balance
         user.balance -= _amount;
         emit UserStakedDecrease(_sender, _amount);
+
+        //update allocation
+        user.allocation -= _amount;
+        totalAllocation -= _amount;
 
         //update totalStaked
         totalStaked -= _amount;
@@ -224,26 +276,20 @@ contract ProfitDistribution is Operator {
         RewardInfo storage reward = rewardInfo[_rewardId];
         require(reward.isActive, "No rewards");
 
-        for (uint256 i = 0; i < stakers.length; ++ i) {
-            address recipient = stakers[i];
-            UserInfo storage user = userInfo[recipient];
-            uint256 poolShare = getPoolShare(recipient);
-            uint256 rewards = poolShare * reward.rewardsPerEpoch / (1e18);
+        //update distributed amount and reward per allocations
+        reward.distributedAmount += reward.rewardsPerEpoch;
 
-            // distribute income proportionally to their staked amount.
+        uint256 thisEpochRPA = reward.rewardsPerEpoch*(1e18)/totalAllocation;
 
-            if(rewards > 0) {
+        reward.LastDistributedAmountPerAlloc = reward.LastDistributedAmountPerAlloc + thisEpochRPA;
+        reward.rewardPerAllocHistory.push(reward.LastDistributedAmountPerAlloc);
 
-                //update pendingRewards
-                user.pendingRewards[_rewardId] += rewards;
-                emit PendingRewardIncrease(recipient,_rewardId, rewards);
-
-                //update totalRewards
-                reward.totalRewards -= rewards;
-                emit RewardDecrease(_rewardId, rewards);
-            }
-
+        if(reward.totalRewards > 0) {
+            //update totalRewards
+            reward.totalRewards -= reward.rewardsPerEpoch;
+            emit RewardDecrease(_rewardId, reward.rewardsPerEpoch);
         }
+
 
         if (reward.totalRewards == 0 || reward.totalRewards < reward.rewardsPerEpoch) {
             reward.isActive = false;
@@ -256,10 +302,16 @@ contract ProfitDistribution is Operator {
         return user.pendingRewards[_rewardId];
     }
 
+    //get pending rewards
+    function getLastSnapShotIndex(uint256 _rewardId, address _user) external view returns(uint256) {
+        UserInfo storage user = userInfo[_user];
+        return user.lastSnapShotIndex[_rewardId];
+    }
+
 
     //collect rewards
 
-    function collectRewards() external {
+    function collectRewards() external updatePendingRewards(msg.sender) {
 
         address _sender = msg.sender;
 
@@ -269,13 +321,12 @@ contract ProfitDistribution is Operator {
         //update pendingRewards and collectRewards
 
         //loop through the reward IDs
-        for(uint256 i = 0; i < rewardInfo.length; ++i)
-        //if pending rewards is not 0
+        for(uint256 i = 0; i < rewardInfo.length; ++i){
+            //if pending rewards is not 0
             if (user.pendingRewards[i] > 0){
 
                 RewardInfo storage reward = rewardInfo[i];
                 uint256 rewardsClaim = user.pendingRewards[i];
-
                 //reset pending rewards
                 user.pendingRewards[i] = 0;
 
@@ -283,11 +334,12 @@ contract ProfitDistribution is Operator {
                 emit PendingRewardClaimed(_sender);
                 reward.token.safeTransfer(_sender, rewardsClaim);
             }
+        }
     }
 
     //get the pool share of a staker
     function getPoolShare(address _user) public view returns(uint256) {
-        return (userInfo[_user].balance * (1e18)) / totalStaked;
+        return (userInfo[_user].allocation * (1e18)) / totalStaked;
     }
 
     function distributeRewards() external onlyOperator {
